12c35b8c33dfeec87e9c15ad62f9c22ab7df77e0
rita <pocyriteko@gmail.com>
2023-03-21 19:21:02 +0700

    add st_f
    добавила конструкторы, набросала апдейт информации о файле

--- state_file.cpp
+++ state_file.cpp
@@ -1,17 +1,56 @@
-class StateFile{
+#include "state_file.h"
+#include <QFile>
+#include <QFileInfo>
 
-public:
-StateFile(Qstring );
-StateFile()
+
+StateFile::StateFile() //конструктор по умолчанию на всякий случай...
 {
-
-//???
+    FName="";
+    FSize=0;
+    ExistStatus=0;
 }
 
-private:
-Qstring FileName;
-qint64 size;
-bool isExist;
+//добавить ли конструктор с путем файла???
+//подумать
 
 
-};
+StateFile::StateFile(const QString &filename)
+{
+    FName=filename; //Обзываем файл
+    QFile watched_file(FName);
+    FSize = watched_file.size(); //определяем размер файла
+    ExistStatus = watched_file.exist(); //проверяем существование
+
+}
+
+
+//     возможно потребуется перезагрузить    =   !!! подумать
+
+
+//геттеры имени, размера, факта существования
+
+QString StateFile::getFName() const
+{
+    return FName;
+}
+
+QString StateFile::getFSize() const
+{
+    return FSize;
+}
+
+QString StateFile::getExistStatus() const
+{
+    return ExistStatus;
+}
+
+
+//update file data
+void udFile()
+{
+    /*!! существует ли файл сейчас? !!
+     * если нет, но раньше да -> сигнал
+     * если да, но раньше нет -> сигнал
+     * если да, но размер отличается -> сигнал
+     */
+}
--- state_file.h
+++ state_file.h
@@ -2,9 +2,31 @@
 #define STATE_FILE_H
 #include <QString>
 #include <QFileInfo>
+#include <QObject>
+
+
+class StateFile : public QObject
+{
+    //Q_OBJECT
+private:
+
+    QString FName;  //Имя файла
+    qint64 FSize;   //Размер файла
+    bool ExistStatus;   //Существование файла
+
+
+public:
+    StateFile(); //конструктор по умолчанию
+    StateFile(const QString & filename); //конструктор
+
+    //геттеры для имени, размера и факта существования
+    QString getFName() const;
+    qint64 getFSize() const;
+    bool getExistStatus() const;
 
 
 
 
+};
 
 #endif // STATE_FILE_H
